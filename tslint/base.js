c0f5897f9a707d61a880b580fe7fd0fec651ec00
Zachary Jones <zachary@thorough.company>
2018-06-06 14:28:20 -0400

    Make linting more strict as part of an experiment.
    Instead of starting from scratch and adding linting rules I am experimenting with turning them all on and then consciously and intentionally turning off ones I disagree with.

--- tslint/base.js
+++ tslint/base.js
@@ -1,36 +1,18 @@
-const path = require('path');
-
-// TODO Tidy these up because some are likely redundant. The extensions
-// were added to an oft-used existing tslint config.
+const path = require("path");
 
 module.exports = {
+  defaultSeverity: "warning",
   extends: [
-    'tslint:latest',
-    path.join(
-      path.dirname(require.resolve('tslint-config-prettier')),
-      'index'
-    )
+    "tslint:all",
+    path.join(path.dirname(require.resolve("tslint-config-prettier")), "index")
   ],
   rules: {
-    align: [true],
-    'arrow-parens': false,
-    'arrow-return-shorthand': [true, 'multiline'],
-    'await-promise': [true],
-    'interface-name': [true, 'always-prefix'],
-    'interface-over-type-literal': [true],
-    'max-classes-per-file': [false],
-    'max-line-length': [true, 140],
-    'member-access': [false],
-    'no-consecutive-blank-lines': [true],
-    'no-console': [false],
-    'no-duplicate-variable': [true, 'check-parameters'],
-    'no-empty-interface': [false],
-    'no-implicit-dependencies': [false],
-    'no-magic-numbers': [true],
-    'no-return-await': [true],
-    'ordered-imports': [false],
-    'prefer-object-spread': [true],
-    'prefer-template': [true, 'allow-single-concat'],
-    'promise-function-async': [true]
+    "prefer-function-over-method": false,
+    // Don't require explicit type definitions. We do not allow implicit
+    // `any` though, so everything does have either an inferred type or an
+    // explicitly defined one.
+    typedef: false,
+    // Enforce that comments begin with a space.
+    "comment-format": [true, "check-space"]
   }
 };
--- tslint/rxjs.js
+++
@@ -1,9 +0,0 @@
-const base = require('./base');
-
-module.exports = {
-  ...base,
-  rules: {
-    ...base.rules,
-    'no-submodule-imports': [...base.rules['no-submodule-imports'], 'rxjs']
-  }
-};
