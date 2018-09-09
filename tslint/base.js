94f8068295ab3deea2a4c5523fcb1ed64e40c642
Zachary Jones <zachary@thorough.company>
2018-09-09 17:44:11 -0400

    code: Refactor to fix undetected issues

--- tslint/base.js
+++ tslint/base.js
@@ -1,20 +1,12 @@
-const path = require('path');
-
 module.exports = {
   defaultSeverity: 'warning',
-  extends: [
-    'tslint-config-airbnb',
-    path.join(
-      path.dirname(require.resolve('tslint-config-prettier')),
-      'index'
-    )
-  ],
+  extends: ['tslint-config-airbnb', 'tslint-config-prettier'],
   rules: {
     'prefer-function-over-method': false,
 
-    // Don't require explicit type definitions. We do not allow
-    // implicit `any` though, so everything does have either an
-    // inferred type or an explicitly defined one.
+    // Don't require explicit type definitions. We do not allow implicit
+    // `any` though, so everything does have either an inferred type or an
+    // explicitly defined one.
     typedef: false,
 
     // Enforce that comments begin with a space.
